bed8461580112845a6bbe54095c7cf003a5890b9
jeremi <Overload.emperor14@gmail.com>
2023-01-03 23:18:55 +0800

    CORS update

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, Request
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
@@ -12,6 +13,23 @@ from load_model import model_load
 model = model_load("pkl/GNB_model.sav")
 
 app = FastAPI()
+origins = [
+    "http://127.0.0.1:8000",
+    "http://127.0.0.1:5500",
+    "http://id.000webhost.com",
+    "https://id.000webhost.com",
+    "http://tsunderesimulatorchat.000webhostapp.com",
+    "https://tsunderesimulatorchat.000webhostapp.com"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
 templates = Jinja2Templates(directory="templates")
